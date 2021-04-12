c6d1dcd34c64c98d01f4c6833d8887e617e2d148
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-04-12 14:28:53 +0200

    uupdate

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,15 @@
 const btnDefil = document.querySelector(".btnDefil");
+var note = document.querySelector(".note").textContent;
+
+
+window.addEventListener("load", function(){
+    for (var i = 0; i < note; i++){
+        console.log(i+1);
+        document.querySelector(".star"+(i+1)).style.fill = "#EF8275";
+    };
+});
+
+btnDefil.addEventListener("onclick", function(){
+    
+});
+
