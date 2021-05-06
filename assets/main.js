6aa72d4c21fd3b0ab5e90e1a6ecbc7b4d69ed372
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-05-06 17:56:35 +0200

    rge

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,36 @@
 const btnDefil = document.querySelectorAll(".btnDefil");
 const stage = document.querySelectorAll(".stage");
-const cardAudiovisuel = document.querySelector(".card audiovisuel");
-const cardGraphisme = document.querySelector(".card graphisme");
-const cardDeveloppement = document.querySelector(".card developpement");
-const cardCommunication = document.querySelector(".card communication");
+const card = document.querySelectorAll(".card");
+
 
 
 btnDefil.forEach(element => {
     element.addEventListener("click", function(){
         this.parentElement.classList.toggle("active");
+    });
+});
+
+card.forEach(element => {
+    element.addEventListener("click", function(){
+        var domaineNum = this.getAttribute("data-domaine");
+
+        var code = document.createTextNode(`<?php 
+        $domaineChoix = `+domaineNum+`;
+        if($domaineChoix){
+           $sql = "SELECT COUNT(id_domaine)
+           FROM stage
+           WHERE id_domaine = $domaineChoix GROUP BY id_domaine";
+           $req = $link -> prepare($sql);
+           $req -> execute();
+           while($data = $req -> fetch()){
+               echo '<script>alert("'.$data["COUNT(id_domaine)"].'")</script>';
+           };
+       }
+   ?>`);
+
+        this.appendChild(code);
+
+        console.log(domaineNum);
     });
 });
 
