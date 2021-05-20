25cf6229d855a0832489cb315bd99657484a6f24
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-05-20 13:49:04 +0200

    qdc

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,22 @@
-const btnDefil = document.querySelectorAll(".btnDefil");
-const stage = document.querySelectorAll(".stage");
-const card = document.querySelectorAll(".card");
+$(document).ready(function(){
 
-
-
-btnDefil.forEach(element => {
-    element.addEventListener("click", function(){
-        this.parentElement.classList.toggle("active");
+    $(".btnDefil").each(function(){
+        $(this).on("click", function(){
+            $(this).parent().toggleClass("active");
+        });
     });
-});
+})
 
-card.forEach(element => {
-    element.addEventListener("click", function(){
-        var domaineNum = this.getAttribute("data-domaine");
 
-        var code = document.createTextNode(`<?php 
-        $domaineChoix = `+domaineNum+`;
-        if($domaineChoix){
-           $sql = "SELECT COUNT(id_domaine)
-           FROM stage
-           WHERE id_domaine = $domaineChoix GROUP BY id_domaine";
-           $req = $link -> prepare($sql);
-           $req -> execute();
-           while($data = $req -> fetch()){
-               echo '<script>alert("'.$data["COUNT(id_domaine)"].'")</script>';
-           };
-       }
-   ?>`);
 
-        this.appendChild(code);
+// card.forEach(element => {
+//     element.addEventListener("click", function(){
+//         var domaineNum = this.getAttribute("data-domaine");
 
-        console.log(domaineNum);
-    });
-});
+//         filtreJS.replaceWith = '<?php $domaineChoix = '+domaineNum+'?>';
+
+//         console.log(domaineNum);
+//     });
+// });
 
 
