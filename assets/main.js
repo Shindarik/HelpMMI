8e7d045140833a3030dc875a4e67a684fd9a5c08
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-06-05 13:34:05 +0200

    qzd

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
 
-    $(window).bind('beforeunload',function(){
+
+    $(window).bind('beforeunload', function () {
 
         $("#filtre3").prop('checked', true);
         $("#filtre2").prop('checked', false);
         $("#filtre1").prop('checked', false);
-   
-   });
+
+    });
 
     var check = false;
     var filtreAnnee = 3;
     var domaineCard = 5;
+    var stagesCount = $(".stages").children().length;
+    var map = L.map('mapid').setView([48.8485022, 2.5764791], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    var long, lat, entreprise, layer = [];
 
     $(".btnDefil").each(function () {
         $(this).on("click", function () {
@@ -30,12 +37,12 @@ $(document).ready(function () {
                 filtreAnnee = $(this).attr("value");
             }
 
-            console.log(domaineCard + " " + filtreAnnee);
-
             $(".card").each(function () {
 
                 $(this).on("click", function () {
 
+                    console.log("check");
+                    
                     $(".stage").each(function () {
 
                         if (domaineCard != 5) {
@@ -138,11 +145,85 @@ $(document).ready(function () {
 
 
 
-            
+
         });
-        
-        
+
+
+
+
     });
-    
+
+    $("#map").on("click", function () {
+
+        setTimeout(function () {
+            map.invalidateSize()
+        }, 100);
+
+        for (var j = 0; j < stagesCount; j++) {
+            if (layer[j] != undefined) {
+                layer[j].remove();
+            }
+        }
+
+
+        for (var i = 0; i < stagesCount; i++) {
+
+
+            if (($(".stages").children().eq(i).attr("data-long") != "") && ($(".stages").children().eq(i).attr("data-lat") != "") && ($(".stages").children().eq(i).css('display') != 'none')) {
+
+
+                if (domaineCard != 5) {
+
+                    if (filtreAnnee != 3) {
+
+                        if (($(".stages").children().eq(i).attr("data-domaine") == domaineCard) && ($(".stages").children().eq(i).attr("data-annee") == filtreAnnee)) {
+                            long = $(".stages").children().eq(i).attr("data-long");
+                            lat = $(".stages").children().eq(i).attr("data-lat");
+                            entreprise = $(".stages").children().eq(i).attr("data-entreprise");
+                        }
+
+                    } else {
+
+                        if ((($(".stages").children().eq(i).attr("data-annee") == 1) || ($(".stages").children().eq(i).attr("data-annee") == 2)) && ($(".stages").children().eq(i).attr("data-domaine") == domaineCard)) {
+                            long = $(".stages").children().eq(i).attr("data-long");
+                            lat = $(".stages").children().eq(i).attr("data-lat");
+                            entreprise = $(".stages").children().eq(i).attr("data-entreprise");
+                        }
+                    }
+
+                } else {
+
+                    if (filtreAnnee != 3) {
+
+                        if ((($(".stages").children().eq(i).attr("data-domaine") == 1) || ($(".stages").children().eq(i).attr("data-domaine") == 2) || ($(".stages").children().eq(i).attr("data-domaine") == 3) || ($(".stages").children().eq(i).attr("data-domaine") == 4)) && ($(".stages").children().eq(i).attr("data-annee") == filtreAnnee)) {
+                            long = $(".stages").children().eq(i).attr("data-long");
+                            lat = $(".stages").children().eq(i).attr("data-lat");
+                            entreprise = $(".stages").children().eq(i).attr("data-entreprise");
+                        }
+
+                    } else {
+                        long = $(".stages").children().eq(i).attr("data-long");
+                        lat = $(".stages").children().eq(i).attr("data-lat");
+                        entreprise = $(".stages").children().eq(i).attr("data-entreprise");
+                    }
+                }
+
+                layer[i] = L.marker([long, lat]).addTo(map)
+                    .bindPopup(entreprise);
+            }
+
+        }
+
+        $("#mapid").show();
+        $(".bgMap").show();
+        $("html").css("overflow", "hidden");
+
+    });
+
+    $(".bgMap span").on("click", function () {
+        $("#mapid").hide();
+        $(".bgMap").hide();
+        $("html").css("overflow", "auto");
+    });
 
 })
